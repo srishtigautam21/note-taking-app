9b26d0b0147e81993c94ba1a02355cdd4b3e00a2
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-04-14 16:06:27 +0530

    completed require Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Mockman from "mockman-js";
+import { RequireAuth } from "./utils/RequireAuth";
 import {
   LandingPage,
   HomePage,
@@ -11,18 +12,42 @@ import {
 } from "./pages";
 
 function App() {
-  // const { pathname } = useLocation();  //future TODO
-
   return (
     <>
-      {/* {pathname !== "/" && <Navbar />} */}
-
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        <Route path='/homePage' element={<HomePage />} />
-        <Route path='/archive' element={<Archive />} />
-        <Route path='/labels' element={<Labels />} />
-        <Route path='/trash' element={<Trash />} />
+        <Route
+          path='/homePage'
+          element={
+            <RequireAuth>
+              <HomePage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/archive'
+          element={
+            <RequireAuth>
+              <Archive />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/labels'
+          element={
+            <RequireAuth>
+              <Labels />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/trash'
+          element={
+            <RequireAuth>
+              <Trash />
+            </RequireAuth>
+          }
+        />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/mockman' element={<Mockman />} />
       </Routes>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,17 @@
 import { useContext, createContext, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
+  const location = useLocation();
   const initialData = {
     email: "adarshbalika@gmail.com",
     password: "adarshBalika123",
   };
+  let from = location.state?.from?.pathname || "/homePage";
   const [loginInfo, setloginInfo] = useState(initialData);
   const [isUserLoggedIn, setUserLoggedIn] = useState(false);
   const loginHandler = async (e, email, password) => {
@@ -18,7 +20,7 @@ const AuthProvider = ({ children }) => {
       const response = await axios.post("/api/auth/login", { email, password });
       localStorage.setItem("token", response.data.encodedToken);
       setUserLoggedIn(true);
-      navigate("/homePage");
+      navigate(from, { replace: true });
     } catch (e) {
       console.error(e);
     }
